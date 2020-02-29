446e5679a88c30c60ea93de389aaf039349be9ca
choknirandev1992 <chokniran@gmail.com>
2020-02-29 17:36:09 +0700

    Adding a Separate Layout to the Admin Area

---
+++ layouts/admin.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+
+    <nuxt />
+  </div>
+</template>
+
+<style>
+
+html{
+  font-family: 'Sarabun';
+}
+
+body {
+    margin: 0;
+}
+
+section.feature-post {
+    padding: 15px;
+}
+</style>
