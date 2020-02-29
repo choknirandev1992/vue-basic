1c4a89a0e45478d2c21b0f1b1fe785070d1b9812
choknirandev1992 <chokniran@gmail.com>
2020-02-29 16:48:35 +0700

    Creating an Admin Section

---
+++ pages/admin/index.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="admin-page">
+     <section class="new-post">
+         <button @click="$router.push('/admin/new-post/')">Create Post</button>
+     </section>
+     <section class="existing-posts">
+        <h1>Existing Posts</h1>
+        <PostList/>
+     </section>
+  </div>
+</template>
+
+<script>
+import PostList from '@/components/Posts/PostList'
+
+export default {
+   components: {
+     PostList
+   }
+}
+</script>
+
+<style scoped>
+.intro {
+  position: relative;
+  padding: 30px;
+  box-sizing: border-box;
+  background-position: center;
+  background-size: cover;
+  background-image: url('~assets/images/original.jpg')
+}
+
+.post-thumnail {
+    height: 200px;
+    background-size: cover;
+    background-position: center center;
+}
+
+.intro h1 {
+    position: static;
+    top: 10%;
+    width: 100%;
+    color: black;
+    background-color: rgb(211, 211, 211);
+    padding: 10px;
+    border-radius: 10px;
+    box-shadow: 3px 3px 3px black;
+    box-sizing: border-box;
+    border: 1px solid black;
+    text-align: center;
+}
+
+@media (min-width: 768px) {
+  .intro h1 {
+    font-size: 2rem;
+  }
+}
+
+.featured-posts {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+
+.post-preview {
+  border: 1px solid #ccc;
+  box-shadow: 0 2px 2px #ccc;
+  background-color: white;
+  width: 90%;
+}
+
+a {
+  text-decoration: none;
+  color: black;
+}
+
+@media (min-width: 850px) {
+  .post-preview {
+    width: 400px;
+    display: block;
+    margin: 0 auto 10px;
+  }
+}
+
+.post-thumbnail {
+  width: 100%;
+  height: 200px;
+  background-position: center;
+  background-size: cover;
+}
+
+.post-content {
+  padding: 10px;
+  text-align: center;
+}
+
+a:hover .post-content,
+a:active .post-content {
+  background-color: #ccc;
+}
+
+.admin-page {
+  padding: 20px;
+}
+
+.new-post {
+  text-align: center;
+  border-bottom: 2px solid #ccc;
+  padding-bottom: 10px;
+}
+
+.existing-posts h1 {
+  text-align: center;
+}
+</style>
